e97b211ea1cc9db121dfcd64049b914ddde5f7f5
nt636@tux.cci.drexel.edu <khanhnguyentrinhthi@gmail.com>
2025-11-18 09:54:34 -0500

    Fix LP

--- optimization/linear-program/graph.py
+++ optimization/linear-program/graph.py
@@ -1,1 +1,21 @@
 # Graph visualization for LPs
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def plot_graph(nodes, edges, flows=None, title="Traffic Network"):
+    G = nx.DiGraph()
+    G.add_nodes_from(nodes)
+    for (i,j), cap in edges.items():
+        label = f"{cap}"
+        if flows and (i,j) in flows:
+            label += f"\n({flows[(i,j)]:.0f})"
+        G.add_edge(i, j, label=label)
+    
+    pos = nx.spring_layout(G, seed=42)
+    edge_labels = nx.get_edge_attributes(G, 'label')
+    
+    plt.figure(figsize=(6,4))
+    nx.draw(G, pos, with_labels=True, node_size=1000, node_color='lightblue', arrowsize=20)
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+    plt.title(title)
+    plt.show()
--- optimization/linear-program/solve.py
+++ optimization/linear-program/solve.py
@@ -1,1 +1,28 @@
 # Solve LP
+# solve.py
+import pulp
+
+def solve_traffic_lp(nodes, edges, sources, sinks):
+    prob = pulp.LpProblem("TrafficSignalOptimization", pulp.LpMaximize)
+
+    x = {e: pulp.LpVariable(f"x_{e[0]}_{e[1]}", lowBound=0) for e in edges}
+    t = {n: pulp.LpVariable(f"t_{n}", lowBound=0, upBound=1) for n in nodes}
+
+    prob += pulp.lpSum(x[e] for e in edges if e[0] in sources)
+
+    for (i,j), cap in edges.items():
+        prob += x[(i,j)] <= cap * t[i]
+
+    internal_nodes = [n for n in nodes if n not in sources+sinks]
+    for n in internal_nodes:
+        incoming = [x[(i,j)] for (i,j) in edges if j==n]
+        outgoing = [x[(i,j)] for (i,j) in edges if i==n]
+        prob += pulp.lpSum(incoming) == pulp.lpSum(outgoing)
+
+
+    prob.solve()
+
+    flow_solution = {e: x[e].varValue for e in edges}
+    timing_solution = {n: t[n].varValue for n in nodes}
+
+    return flow_solution, timing_solution
